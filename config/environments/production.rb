ea30e90324315fe2d6528d1aa59da723283da61a
Kazuma Wada <kazumawada.aus@gmail.com>
2024-12-01 23:10:28 +0700

    try render23

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,10 +1,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  #一時的にhostを無効化。これでデプロイできたら原因はhostということになる。
-  config.hosts.clear
   ##render用##
-  ##config.hosts << "onedayoneline.onrender.com"
+  config.hosts << "onedayoneline-1.onrender.com"
 
   # Settings specified here will take precedence over those in config/application.rb.
 
