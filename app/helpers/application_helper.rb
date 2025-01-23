55a9419f6740fe3a309f440e6004aab620264ad8
Kazuma Wada <kazumawada.aus@gmail.com>
2025-01-23 13:23:57 +0900

    add x card

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,26 +1,2 @@
 module ApplicationHelper
-    def default_meta_tags
-        {
-          site: '詐欺師の手帳',
-          title: '詐欺師の手帳',
-          reverse: true,
-          charset: 'utf-8',
-          description: '詐欺被害の未然防止を目的としたアプリです',
-          canonical: request.original_url,
-          og: {
-            site_name: '詐欺師の手帳',
-            title: '詐欺師の手帳',
-            description: '詐欺被害の未然防止を目的としたアプリです',
-            type: 'website',
-            url: request.original_url,
-            image: image_url('write.png'),
-            local: 'ja-JP'
-          },
-          twitter: {
-            card: 'summary_large_image',
-            site: '@https://x.com/yukimura877',
-            image: image_url('write.png')
-          }
-        }
-      end
 end
