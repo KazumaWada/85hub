19b58627ea85f8a2c4b327ab972c1fedc87e662c
KazumaWada <kazumawadaa@gmail.com>
2025-03-26 00:26:38 +0900

    add initial card

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_03_21_142743) do
+ActiveRecord::Schema[7.0].define(version: 2025_03_25_143829) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_03_21_142743) do
     t.text "answer"
     t.integer "correct_num", default: 0
     t.string "tags"
+    t.boolean "initial_card", default: false
     t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
